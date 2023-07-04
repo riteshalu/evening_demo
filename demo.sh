cf64df15515415064f3affc8686ac1850ca0c29d
riteshalu <riteshrajagrawal@gmail.com>
2023-07-04 16:30:45 +0000

    New Change

--- demo.sh
+++ demo.sh
@@ -4,5 +4,13 @@ echo "We edited this file"
 echo "Edited from Local Git"
 echo "Edited from github"
 echo "Editing file from new branch"
+<<<<<<< Updated upstream
 echo "Rebase"
 echo "Git Main"
+=======
+<<<<<<< HEAD
+=======
+echo "Rebase"
+echo "Git Main"
+>>>>>>> c0663a4 (main Branch)
+>>>>>>> Stashed changes
